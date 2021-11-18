333ae2f9b82d078a73dbbbfa6f52df85b86c867c
npaulat <34585535+npaulat@users.noreply.github.com>
2021-11-18 12:45:59 -0600

    Add files via upload

---
+++ final_paste_sp_table.sh
@@ -0,0 +1,518 @@
+#!/bin/bash 
+#SBATCH --job-name=te_counts
+#SBATCH --output=%x.%j.out
+#SBATCH --error=%x.%j.err
+#SBATCH --partition=nocona
+#SBATCH --mem-per-cpu=100G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+
+cd /lustre/scratch/npaulat/RayLib-Masking/te-counts/
+
+for i in *_HT; do awk -F ' ' '{print $3}' ${i} > /lustre/scratch/npaulat/RayLib-Masking/te-counts/3rd_column/${i}2; done
+
+sleep 1
+cd 3rd_column
+
+cp DasNov_HT2 test1
+sleep 1
+paste test1 TolMat_HT2 > test2
+sleep 1
+paste test2 ChaVel_HT2 > test3
+sleep 1
+paste test3 TamTet_HT2 > test4
+sleep 1
+paste test4 MyrTri_HT2 > test5
+sleep 1
+paste test5 BraVar_HT2 > test6
+sleep 1
+paste test6 ChoDid_HT2 > test7
+sleep 1
+paste test7 ChoHof_HT2 > test8
+sleep 1
+paste test8 TriMan_HT2 > test9
+sleep 1
+paste test9 LoxAfr_HT2 > test10
+sleep 1
+paste test10 ProCap_HT2 > test11
+sleep 1
+paste test11 HetBru_HT2 > test12
+sleep 1
+paste test12 OryAfe_HT2 > test13
+sleep 1
+paste test13 EleEdw_HT2 > test14
+sleep 1
+paste test14 ChrAsi_HT2 > test15
+sleep 1
+paste test15 MicTal_HT2 > test16
+sleep 1
+paste test16 EchTel_HT2 > test17
+sleep 1
+paste test17 TupTan_HT2 > test18
+sleep 1
+paste test18 TupChi_HT2 > test19
+sleep 1
+paste test19 GalVar_HT2 > test20
+sleep 1
+paste test20 NycCou_HT2 > test21
+sleep 1
+paste test21 OtoGar_HT2 > test22
+sleep 1
+paste test22 DauMad_HT2 > test23
+sleep 1
+paste test23 LemCat_HT2 > test24
+sleep 1
+paste test24 EulFul_HT2 > test25
+sleep 1
+paste test25 EulFla_HT2 > test26
+sleep 1
+paste test26 ProCoq_HT2 > test27
+sleep 1
+paste test27 IndInd_HT2 > test28
+sleep 1
+paste test28 CheMed_HT2 > test29
+sleep 1
+paste test29 MicMur_HT2 > test30
+sleep 1
+paste test30 MirCoq_HT2 > test31
+sleep 1
+paste test31 PitPit_HT2 > test32
+sleep 1
+paste test32 CalDon_HT2 > test33
+sleep 1
+paste test33 AloPal_HT2 > test34
+sleep 1
+paste test34 AteGeo_HT2 > test35
+sleep 1
+paste test35 AotNan_HT2 > test36
+sleep 1
+paste test36 SagImp_HT2 > test37
+sleep 1
+paste test37 CalJac_HT2 > test38
+sleep 1
+paste test38 SaiBol_HT2 > test39
+sleep 1
+paste test39 CebAlb_HT2 > test40
+sleep 1
+paste test40 CebCap_HT2 > test41
+sleep 1
+paste test41 NomLeu_HT2 > test42
+sleep 1
+paste test42 PonAbe_HT2 > test43
+sleep 1
+paste test43 GorGor_HT2 > test44
+sleep 1
+paste test44 HomSap_HT2 > test45
+sleep 1
+paste test45 PanTro_HT2 > test46
+sleep 1
+paste test46 PanPan_HT2 > test47
+sleep 1
+paste test47 PilTep_HT2 > test48
+sleep 1
+paste test48 ColAng_HT2 > test49
+sleep 1
+paste test49 SemEnt_HT2 > test50
+sleep 1
+paste test50 NasLar_HT2 > test51
+sleep 1
+paste test51 PygNem_HT2 > test52
+sleep 1
+paste test52 RhiRox_HT2 > test53
+sleep 1
+paste test53 RhiBie_HT2 > test54
+sleep 1
+paste test54 CerNeg_HT2 > test55
+sleep 1
+paste test55 EryPat_HT2 > test56
+sleep 1
+paste test56 ChlSab_HT2 > test57
+sleep 1
+paste test57 PapAnu_HT2 > test58
+sleep 1
+paste test58 ManLeu_HT2 > test59
+sleep 1
+paste test59 CerAty_HT2 > test60
+sleep 1
+paste test60 MacNem_HT2 > test61
+sleep 1
+paste test61 MacMul_HT2 > test62
+sleep 1
+paste test62 MacFas_HT2 > test63
+sleep 1
+paste test63 OchPri_HT2 > test64
+sleep 1
+paste test64 OryCun_HT2 > test65
+sleep 1
+paste test65 LepAme_HT2 > test66
+sleep 1
+paste test66 GliGli_HT2 > test67
+sleep 1
+paste test67 GraMur_HT2 > test68
+sleep 1
+paste test68 MusAve_HT2 > test69
+sleep 1
+paste test69 AplRuf_HT2 > test70
+sleep 1
+paste test70 XerIna_HT2 > test71
+sleep 1
+paste test71 MarMar_HT2 > test72
+sleep 1
+paste test72 SpeDau_HT2 > test73
+sleep 1
+paste test73 IctTri_HT2 > test74
+sleep 1
+paste test74 CteGun_HT2 > test75
+sleep 1
+paste test75 HysCri_HT2 > test76
+sleep 1
+paste test76 PetTyp_HT2 > test77
+sleep 1
+paste test77 ThrSwi_HT2 > test78
+sleep 1
+paste test78 HetGla_HT2 > test79
+sleep 1
+paste test79 FukDam_HT2 > test80
+sleep 1
+paste test80 ChiLan_HT2 > test81
+sleep 1
+paste test81 DinBra_HT2 > test82
+sleep 1
+paste test82 OctDeg_HT2 > test83
+sleep 1
+paste test83 CteSoc_HT2 > test84
+sleep 1
+paste test84 MyoCoy_HT2 > test85
+sleep 1
+paste test85 CapPil_HT2 > test86
+sleep 1
+paste test86 CunPac_HT2 > test87
+sleep 1
+paste test87 DasPun_HT2 > test88
+sleep 1
+paste test88 DolPat_HT2 > test89
+sleep 1
+paste test89 HydHyd_HT2 > test90
+sleep 1
+paste test90 CavApe_HT2 > test91
+sleep 1
+paste test91 CavTsc_HT2 > test92
+sleep 1
+paste test92 CavPor_HT2 > test93
+sleep 1
+paste test93 CasCan_HT2 > test94
+sleep 1
+paste test94 PerLon_HT2 > test95
+sleep 1
+paste test95 DipSte_HT2 > test96
+sleep 1
+paste test96 DipOrd_HT2 > test97
+sleep 1
+paste test97 ZapHud_HT2 > test98
+sleep 1
+paste test98 JacJac_HT2 > test99
+sleep 1
+paste test99 AllBul_HT2 > test100
+sleep 1
+paste test100 RhiPru_HT2 > test101
+sleep 1
+paste test101 NanGal_HT2 > test102
+sleep 1
+paste test102 CriGam_HT2 > test103
+sleep 1
+paste test103 AcoCah_HT2 > test104
+sleep 1
+paste test104 MerUng_HT2 > test105
+sleep 1
+paste test105 PsaObe_HT2 > test106
+sleep 1
+paste test106 RatNor_HT2 > test107
+sleep 1
+paste test107 MusPah_HT2 > test108
+sleep 1
+paste test108 MusCar_HT2 > test109
+sleep 1
+paste test109 MusMus_HT2 > test110
+sleep 1
+paste test110 MusSpr_HT2 > test111
+sleep 1
+paste test111 MesAur_HT2 > test112
+sleep 1
+paste test112 CriGri_HT2 > test113
+sleep 1
+paste test113 SigHis_HT2 > test114
+sleep 1
+paste test114 PerMan_HT2 > test115
+sleep 1
+paste test115 OnyTor_HT2 > test116
+sleep 1
+paste test116 OndZib_HT2 > test117
+sleep 1
+paste test117 MicOch_HT2 > test118
+sleep 1
+paste test118 EllTal_HT2 > test119
+sleep 1
+paste test119 EllLut_HT2 > test120
+sleep 1
+paste test120 SolPar_HT2 > test121
+sleep 1
+paste test121 EriEur_HT2 > test122
+sleep 1
+paste test122 CroInd_HT2 > test123
+sleep 1
+paste test123 SorAra_HT2 > test124
+sleep 1
+paste test124 UroGra_HT2 > test125
+sleep 1
+paste test125 ConCri_HT2 > test126
+sleep 1
+paste test126 ScaAqu_HT2 > test127
+sleep 1
+paste test127 MacSob_HT2 > test128
+sleep 1
+paste test128 EidHel_HT2 > test129
+sleep 1
+paste test129 RouAeg_HT2 > test130
+sleep 1
+paste test130 PteVam_HT2 > test131
+sleep 1
+paste test131 PteAle_HT2 > test132
+sleep 1
+paste test132 RhiFer_HT2 > test133
+sleep 1
+paste test133 RhiSin_HT2 > test134
+sleep 1
+paste test134 HipGal_HT2 > test135
+sleep 1
+paste test135 HipArm_HT2 > test136
+sleep 1
+paste test136 MegLyr_HT2 > test137
+sleep 1
+paste test137 CraTho_HT2 > test138
+sleep 1
+paste test138 MurFea_HT2 > test139
+sleep 1
+paste test139 MyoLuc_HT2 > test140
+sleep 1
+paste test140 MyoBra_HT2 > test141
+sleep 1
+paste test141 MyoDav_HT2 > test142
+sleep 1
+paste test142 MyoMyo_HT2 > test143
+sleep 1
+paste test143 PipPip_HT2 > test144
+sleep 1
+paste test144 PipKuh_HT2 > test145
+sleep 1
+paste test145 aPal_HT2 > test146
+sleep 1
+paste test146 LasBor_HT2 > test147
+sleep 1
+paste test147 eFus_HT2 > test148
+sleep 1
+paste test148 MinSch_HT2 > test149
+sleep 1
+paste test149 MinNat_HT2 > test150
+sleep 1
+paste test150 MolMol_HT2 > test151
+sleep 1
+paste test151 TadBra_HT2 > test152
+sleep 1
+paste test152 NocLep_HT2 > test153
+sleep 1
+paste test153 PtePar_HT2 > test154
+sleep 1
+paste test154 MorBla_HT2 > test155
+sleep 1
+paste test155 aJam_HT2 > test156
+sleep 1
+paste test156 sHon_HT2 > test157
+sleep 1
+paste test157 CarPer_HT2 > test158
+sleep 1
+paste test158 AnoCau_HT2 > test159
+sleep 1
+paste test159 pHas_HT2 > test160
+sleep 1
+paste test160 PhyDis_HT2 > test161
+sleep 1
+paste test161 TonSau_HT2 > test162
+sleep 1
+paste test162 DesRot_HT2 > test163
+sleep 1
+paste test163 MicHir_HT2 > test164
+sleep 1
+paste test164 EquAsi_HT2 > test165
+sleep 1
+paste test165 EquCab_HT2 > test166
+sleep 1
+paste test166 EquPrz_HT2 > test167
+sleep 1
+paste test167 TapInd_HT2 > test168
+sleep 1
+paste test168 TapTer_HT2 > test169
+sleep 1
+paste test169 DicSum_HT2 > test170
+sleep 1
+paste test170 DicBic_HT2 > test171
+sleep 1
+paste test171 CerSim_HT2 > test172
+sleep 1
+paste test172 CerCot_HT2 > test173
+sleep 1
+paste test173 ManJav_HT2 > test174
+sleep 1
+paste test174 ManPen_HT2 > test175
+sleep 1
+paste test175 ParHer_HT2 > test176
+sleep 1
+paste test176 HyaHya_HT2 > test177
+sleep 1
+paste test177 CryFer_HT2 > test178
+sleep 1
+paste test178 SurSur_HT2 > test179
+sleep 1
+paste test179 HelPar_HT2 > test180
+sleep 1
+paste test180 MunMun_HT2 > test181
+sleep 1
+paste test181 PanTig_HT2 > test182
+sleep 1
+paste test182 PanPar_HT2 > test183
+sleep 1
+paste test183 PanOnc_HT2 > test184
+sleep 1
+paste test184 AciJub_HT2 > test185
+sleep 1
+paste test185 PumCon_HT2 > test186
+sleep 1
+paste test186 FelCat_HT2 > test187
+sleep 1
+paste test187 FelNig_HT2 > test188
+sleep 1
+paste test188 VulLag_HT2 > test189
+sleep 1
+paste test189 LycPic_HT2 > test190
+sleep 1
+paste test190 CanLup_HT2 > test191
+sleep 1
+paste test191 CanFam_HT2 > test192
+sleep 1
+paste test192 AilMel_HT2 > test193
+sleep 1
+paste test193 UrsMar_HT2 > test194
+sleep 1
+paste test194 ZalCal_HT2 > test195
+sleep 1
+paste test195 OdoRos_HT2 > test196
+sleep 1
+paste test196 NeoSch_HT2 > test197
+sleep 1
+paste test197 LepWed_HT2 > test198
+sleep 1
+paste test198 MirAng_HT2 > test199
+sleep 1
+paste test199 SpiGra_HT2 > test200
+sleep 1
+paste test200 AilFul_HT2 > test201
+sleep 1
+paste test201 MelCap_HT2 > test202
+sleep 1
+paste test202 MusPut_HT2 > test203
+sleep 1
+paste test203 PteBra_HT2 > test204
+sleep 1
+paste test204 EnhLut_HT2 > test205
+sleep 1
+paste test205 VicPac_HT2 > test206
+sleep 1
+paste test206 CamDro_HT2 > test207
+sleep 1
+paste test207 CamFer_HT2 > test208
+sleep 1
+paste test208 CamBac_HT2 > test209
+sleep 1
+paste test209 CatWag_HT2 > test210
+sleep 1
+paste test210 SusScr_HT2 > test211
+sleep 1
+paste test211 HipAmp_HT2 > test212
+sleep 1
+paste test212 EubJap_HT2 > test213
+sleep 1
+paste test213 EscRob_HT2 > test214
+sleep 1
+paste test214 BalBon_HT2 > test215
+sleep 1
+paste test215 BalAcu_HT2 > test216
+sleep 1
+paste test216 KogBre_HT2 > test217
+sleep 1
+paste test217 PlaGan_HT2 > test218
+sleep 1
+paste test218 MesBid_HT2 > test219
+sleep 1
+paste test219 ZipCav_HT2 > test220
+sleep 1
+paste test220 LipVex_HT2 > test221
+sleep 1
+paste test221 IniGeo_HT2 > test222
+sleep 1
+paste test222 PonBla_HT2 > test223
+sleep 1
+paste test223 TurTru_HT2 > test224
+sleep 1
+paste test224 OrcOrc_HT2 > test225
+sleep 1
+paste test225 NeoAsi_HT2 > test226
+sleep 1
+paste test226 PhoPho_HT2 > test227
+sleep 1
+paste test227 DelLeu_HT2 > test228
+sleep 1
+paste test228 MonMon_HT2 > test229
+sleep 1
+paste test229 TraJav_HT2 > test230
+sleep 1
+paste test230 AntAme_HT2 > test231
+sleep 1
+paste test231 OkaJoh_HT2 > test232
+sleep 1
+paste test232 GirTip_HT2 > test233
+sleep 1
+paste test233 ElaDav_HT2 > test234
+sleep 1
+paste test234 OdoVir_HT2 > test235
+sleep 1
+paste test235 RanTar_HT2 > test236
+sleep 1
+paste test236 MosMos_HT2 > test237
+sleep 1
+paste test237 BubBub_HT2 > test238
+sleep 1
+paste test238 BisBis_HT2 > test239
+sleep 1
+paste test239 BosMut_HT2 > test240
+sleep 1
+paste test240 BosTau_HT2 > test241
+sleep 1
+paste test241 BosInd_HT2 > test242
+sleep 1
+paste test242 SaiTat_HT2 > test243
+sleep 1
+paste test243 BeaHun_HT2 > test244
+sleep 1
+paste test244 PanHod_HT2 > test245
+sleep 1
+paste test245 HemHyl_HT2 > test246
+sleep 1
+paste test246 OviAri_HT2 > test247
+sleep 1
+paste test247 OviCan_HT2 > test248
+sleep 1
+paste test248 AmmLer_HT2 > test249
+sleep 1
+paste test249 CapHir_HT2 > test250
+sleep 1
+paste test250 CapAeg_HT2 > test251
+sleep 1
